e2b2e97a2eef82b334610653f06f3da419376929
Ithalocaires <ithalop.caires@gmail.com>
2023-05-26 21:58:36 -0300

    Estilização

--- src/componentes/chart/chart.js
+++ src/componentes/chart/chart.js
@@ -8,7 +8,8 @@ import {
     LinearScale,
     PointElement,
  } from "chart.js";
-
+import { DateRangePicker } from 'rsuite';
+import {useState} from 'react'
  import './chart.css'
 
  ChartJS.register(
@@ -20,34 +21,73 @@ import {
 
 const LineChart = () => {
 
-    const DataSemana1 ={
+
+  var dataMensal1 = [9047, 5411, 6821, 3999, 5851, 5992, 2809, 8148, 6182, 8950, 7758, 9080];
+  var dataMensal2 = [7715, 6568, 6450, 8719, 5475, 4656, 8880, 2835, 8640, 4854, 6885, 3617];
+  var dataMensal3 = [5246, 5378, 4181, 9465, 9527, 6906, 5878, 5154, 4562, 6111, 8323, 4511];
+  var dataSemana1 = [189, 145, 81, 147, 161, 130, 94]
+  var dataSemana1 = [141, 154, 144, 164, 122, 117, 176]
+  var dataSemana1 = [119, 170, 92, 181, 84, 185, 172]
+  var totalMes = [80048, 75294, 75242];
+  var totalSemana = [947, 1018, 1003];
+
+    const DataMeses1 ={
         labels  : ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' ],
         datasets: [{
             label: 'Sensor 1',
-            data: [83, 212, 139, 258, 143, 718, 894, 189, 568, 132, 594, 154],
-            backgroundColor: '#36A2EB',
-            borderColor: '#36A2EB',
-            pointBorderColor: '#36A2EB',
+            data: dataMensal1,
+            backgroundColor: 'rgb(80, 176, 158)',
+            borderColor: 'rgb(80, 176, 158)',
+            pointBorderColor: 'rgb(80, 176, 158)',
             fill: false,
             },
             {
               label: 'Sensor 2',
-              data: [325, 260, 340, 123, 98, 312, 142, 147, 695, 452, 124, 257],
-              backgroundColor: '#FF6283',
-              borderColor: '#FF6283',
-              pointBorderColor: '#FF6283',
+              data: dataMensal2,
+              backgroundColor: 'rgb(54, 162, 235)',
+              borderColor: 'rgb(54, 162, 235)',
+              pointBorderColor: 'rgb(54, 162, 235)',
               fill: false,
             },
             {
               label: 'Sensor 3',
-              data: [422, 124, 139, 258, 143, 151, 165, 156, 489, 359, 523, 474],
-              backgroundColor: '#4ABFBF',
-              borderColor: '#4ABFBF',
-              pointBorderColor: '#4ABFBF',
+              data: dataMensal3,
+              backgroundColor: 'rgb(255, 205, 86)',
+              borderColor: 'rgb(255, 205, 86)',
+              pointBorderColor: 'rgb(255, 205, 86)',
               fill: false,
             }
           ]
         }
+
+        const Data ={
+          labels  : ['Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado', 'Domingo' ],
+          datasets: [{
+              label: 'Sensor 1',
+              data: dataMensal1,
+              backgroundColor: '#36A2EB',
+              borderColor: '#36A2EB',
+              pointBorderColor: '#36A2EB',
+              fill: false,
+              },
+              {
+                label: 'Sensor 2',
+                data: dataMensal2,
+                backgroundColor: 'rgb(54, 162, 235)',
+                borderColor: 'rgb(54, 162, 235)',
+                pointBorderColor: 'rgb(54, 162, 235)',
+                fill: false,
+              },
+              {
+                label: 'Sensor 3',
+                data: dataMensal3,
+                backgroundColor: '#4ABFBF',
+                borderColor: '#4ABFBF',
+                pointBorderColor: '#4ABFBF',
+                fill: false,
+              }
+            ]
+          }
 
     const DataDoughnut = {
       labels: [
@@ -57,12 +97,13 @@ const LineChart = () => {
       ],
       datasets: [{
         label: 'Litros Gastos',
-        data: [4084, 3275, 3403],
+        data: totalMes,
         backgroundColor: [
-          'rgb(255, 99, 132)',
+          'rgb(80, 176, 158)',
           'rgb(54, 162, 235)',
           'rgb(255, 205, 86)'
         ],
+        borderColor: '#282F3C',
         hoverOffset: 4
       }]
     };
@@ -72,11 +113,41 @@ const LineChart = () => {
         plugins: {
           legend: {
             position: 'top',
+            color: 'rgb(255, 255, 255)',
+            labels :{
+              render: 'label',
+              fontColor: ['rgb(255, 255, 255)', 'rgb(255, 255, 255)', 'rgb(255, 255, 255)'],
+  
+  
+            },
           },
+         
           title: {
             display: true,
-            text: 'Sensor 1',
+            text: 'Sensores',
+            color: 'rgb(255, 255, 255)',
           },
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: "white"
+            },
+            grid: {
+              display: false,
+            },
+            
+          },
+          y: {
+            ticks: {
+              color: "white",
+            },
+            grid: {
+              color: '#717171',
+              lineWidth: 0.3,
+            },
+            beginAtZero: true
+          }
         },
       };
 
@@ -85,19 +156,51 @@ const LineChart = () => {
         plugins: {
           legend: {
             display: false,
+            color: 'rgb(255, 255, 255)',
+            labels :{
+              fontColor: 'rgb(255, 255, 255)',
+              fontSize: 1,
+            },
           },
           title: {
             display: true,
             text: 'Controle de gasto de água',
+            color: 'rgb(255, 255, 255)',
           },
+          
         },
       };
+
+    
 
       const SelectOpt = [
         {value: 'Semanal', label:'Semanal'},
         {value: 'Mensal', label:'Mensal'}
 
       ]
+
+	  const [selectedOpt, setSelectedOpt] = useState();
+
+	  const AltChart = (mensalidade) =>{
+		setSelectedOpt(mensalidade.target.value)
+	  }
+
+      const SelectMês = [
+        {value: '1', label:'Janeiro'},
+        {value: '2', label:'Fevereiro'},
+        {value: '3', label:'Março'},
+        {value: '4', label:'Abril'},
+        {value: '5', label:'Maio'},
+        {value: '6', label:'Junho'},
+        {value: '7', label:'Julho'},
+        {value: '8', label:'Agosto'},
+        {value: '9', label:'Setembro'},
+        {value: '10', label:'Outubro'},
+        {value: '11', label:'Novembro'},
+        {value: '12', label:'Dezembro'},
+      ]
+
+    
 
     return (
     <>
@@ -109,17 +212,25 @@ const LineChart = () => {
         
         ></Doughnut>
         <hr/>
-        <Select className="Select1" options={SelectOpt}></Select>
+        <Select className="Select1" options={SelectOpt} onChange={(value)=> AltChart(value)}></Select>
+		{
+			selectedOpt?
+        <DateRangePicker localeText={{ start: 'Check-in', end: 'Check-out' }} />
+			:
+        <DateRangePicker className="datePicker" hoverRange="month" ranges={[]} />
+		}
+
+
+
 
       </div>
       <div className="lineChart">
           <Line  
-          data={DataSemana1}
+          data={DataMeses1}
           options={OptSensor1}
           className="line"
           >
           </Line>
-          <Select className="Select2" options={SelectOpt}></Select>
 
       </div>
     </>
