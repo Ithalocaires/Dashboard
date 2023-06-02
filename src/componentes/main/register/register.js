c9fa28d369f59d93f2756ac80d719039276c14c9
Ithalocaires <ithalop.caires@gmail.com>
2023-06-01 23:44:12 -0300

    Sensor register screen

--- src/componentes/main/register/register.js
+++ src/componentes/main/register/register.js
@@ -3,17 +3,32 @@
   import { TfiPlus, TfiTrash } from "react-icons/tfi";
   import React, { useState } from 'react';
   import Modal from "react-modal"
-  
-
+  import Switch from "react-switch";
   
   Modal.setAppElement('#root');
     
   function Register() {
 
+
+    const[sensors, setSensors] = useState([
+      {id: 1, name: 'Sensor 1', status: true},
+      {id: 2, name: 'Sensor 2', status: true},
+      {id: 3, name: 'Sensor 3', status: true},
+      {id: 4, name: 'Sensor 4', status: false},
+      {id: 5, name: 'Sensor 5', status: false},
+      {id: 6, name: 'Sensor 6', status: false},
+    ]);
    
     const [sensorName, setSensorName] = useState();
     const [sensorStatus, setSensorStatus] = useState(true);
     const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = nextChecked => {
+      setChecked(nextChecked);
+      auxSensors = [...sensors]
+    };
 
     function openModal(){
       setModalIsOpen(true);
@@ -22,18 +37,13 @@
     function closeModal(){
       setModalIsOpen(false);
     }
-
-    const sensors = [
-      {id: 1, name: 'Sensor 1', status: true},
-      {id: 2, name: 'Sensor 2', status: true},
-      {id: 3, name: 'Sensor 3', status: true},
-      {id: 4, name: 'Sensor 4', status: false},
-      {id: 5, name: 'Sensor 5', status: false},
-      {id: 6, name: 'Sensor 6', status: false},
-    ];
-
-    const listItems = sensors.map(({sensors, index, name, status}) =>
-    <li key={index} className="list"> {name} 
+  
+    function handleStatus() {
+      setSensorStatus(false)
+    }
+    
+    const listItems = sensors.map(({id, name, status}) =>
+    <li key={id} className="list"> {name} 
       <h7 className="statusText">status: 
         {status?
         <h7 className="statusON"> ON </h7>
@@ -42,9 +52,24 @@
         }
       </h7>
       <a className="deleteSensor">   
-      <TfiTrash size={20} className="deleteIcon"></TfiTrash>
-      </a> 
-    </li>
+        <TfiTrash size={20} className="deleteIcon"></TfiTrash>  
+      </a>
+      <Switch
+        onChange={handleChange}
+        checked={status}
+        onColor="#86d3ff"
+        onHandleColor="#2693e6"
+        handleDiameter={20}
+        uncheckedIcon={false}
+        checkedIcon={false}
+        boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+        activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+        height={15}
+        width={40}
+        className="react-switch"
+        id="material-switch"
+      /> 
+    </li> 
   );
 
     return (
@@ -54,22 +79,21 @@
               <h1 className="registerTitle">
                 Registrar Sensores
                 <a onClick={openModal} className="openModal">
-                <TfiPlus color="white" size={25} className="addIcon" onClick={openModal}  ></TfiPlus>
+                  <TfiPlus color="white" size={25} className="addIcon" onClick={openModal}  ></TfiPlus>
                 </a>
-
                 <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                contentLabel="Cadastro de Sensores"
-                overlayClassName='modal-overlay'
-                className='modal-content'
+                  isOpen={modalIsOpen}
+                  onRequestClose={closeModal}
+                  contentLabel="Cadastro de Sensores"
+                  overlayClassName='modal-overlay'
+                  className='modal-content'
                 >
                <label className="label">
                   Nome do sensor:
-                  <input type="email" name="name" placeholder="Nome do sensor" />
-                  <button className="confirm" onClick={closeModal}> Confirmar </button>
+                  <input id="nome" value={sensorName} className="sensorNome" type="text" name="name" placeholder="Nome do sensor" />
+                  <button className="confirm" onClick={(e) => setSensorName(e.target.value)}> Confirmar </button>
+                  {console.log(sensorName)}
                </label>
-
                 </Modal>
               </h1>
               <div className="registerContent">
@@ -78,6 +102,9 @@
           </div>
       </div>
     );
+
+    
+    
   }
   
   export default Register;
